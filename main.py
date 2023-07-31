816cd12d9536c846b8c56bae3d600999e368e9bb
Murli Khaire <115461606+Murli0399@users.noreply.github.com>
2023-07-31 18:19:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,94 +1,160 @@
-itemsList = [
-    {"id": 1, "name": "Samosa", "price": 15, "availability": "Yes"},
-    {"id": 2, "name": "Chips", "price": 20, "availability": "Yes"},
-    {"id": 3, "name": "Lassi", "price": 30, "availability": "Yes"}
-]
+from re import match
+from termcolor import colored
+from tabulate import tabulate
+menu = {}
+orders = {}
+reviewOrders = []
+viewMenu = []
 
-soldSnacks = []
+def add():
+    id = str(len(menu) + 1)
+    if id not in menu :
+        id = str(len(menu) + 1)
+    else :
+        id = str(len(menu))
+    name = input(colored("Enter Dish name: ", "blue"))
+    price = input(colored("Enter Dish price: ", "blue"))
+    if not match(r'^\d+$', price) :
+        print(colored("Price can be numbers only!", "red"))
+        return add()
+    stock = input(colored("Enter quantity: ", "blue"))
+    if not match(r'^\d+$', stock) :
+        print(colored("Stock can be numbers only!", "red"))
+        return add()
+    
+    menu[id] = {
+        "name": name,
+        "price": int(price),
+        "stock": int(stock)
+    }
+    viewMenu.append({
+        "id":id,
+        "name": name,
+        "price": int(price),
+        "stock": int(stock)
+    })
 
-def generate_unique_id():
-    max_id = max(item["id"] for item in itemsList) if itemsList else 0
-    return max_id + 1
+    print(colored(f"{name} has been added!", "green"))
 
-def display_menu():
-    print("***** Mumbai Munchies: The Canteen Chronicle *****")
-    print("1. View Inventory")
-    print("2. Add Snack")
-    print("3. Update Snack Availability")
-    print("4. Sell/Remove Snack")
-    print("5. View Sold Snacks")
-    print("6. Exit")
+def remove():
+    print("Enter the Dish ID to remove")
+    dish = input(colored("Enter Dish ID","blue"))
+    if (dish not in menu) :
+        print(colored("Please enter valid ID!","red"))
+        return remove()
+    print(colored(f"{menu[dish]['name']} has been removed!","green"))
+    del menu[dish]
+    viewMenu.pop(int(dish)-1)
 
-def view_inventory():
-    print("***** Snack Inventory *****")
-    for item in itemsList:
-        print(
-            f"ID: {item['id']}\tName: {item['name']}\tPrice: {item['price']}\tAvailability: {item['availability']}")
-    print()
+def update():
+    print("Enter the Dish ID to update quantity")
+    dish = input(colored("Enter Dish ID","blue"))
+    if (dish not in menu) :
+        print(colored("Please enter valid ID!","red"))
+        return update()
+    quantity = input(colored("Enter updated quantity", "blue"))
+    if not match(r'^\d+$', quantity) :
+        print(colored("Stock can be numbers only!", "red"))
+        return update()
+    
+    menu[dish]["stock"] = int(quantity)
+    print(colored("Stock has been updated!","green"))
+    viewMenu[int(dish)-1]['stock'] = int(quantity)
 
-def add_snack():
-    name = input("Enter snack name: ")
-    price = float(input("Enter snack price: "))
-    availability = "Yes"
-    item_id = generate_unique_id()
-    item = {"id": item_id, "name": name,
-            "price": price, "availability": availability}
-    itemsList.append(item)
-    print("Snack added successfully!\n")
+def order():
+    print("Enter the Dish ID to order")
+    dish = input(colored("Enter Dish ID","blue"))
+    if (dish not in menu) :
+        print(colored("Please enter valid ID!","red"))
+        return order()
+    quantity = input(colored("Enter Quantity","blue"))
+    if int(quantity) > menu[dish]['stock'] :
+        print(colored(f"Only {menu[dish]['stock']} pcs of {menu[dish]['name']} is available","red"))
+        return order()
+    id = str(len(orders) + 1)
+    orders[id] = {
+        "name": menu[dish]['name'],
+        "price": menu[dish]['price'],
+        "quantity": int(quantity),
+        "status": "Received"
+    }
+    reviewOrders.append({
+        "id":id,
+        "name": menu[dish]['name'],
+        "price": menu[dish]['price'],
+        "quantity": int(quantity),
+        "status": "Received"
+    })
+    menu[dish]['stock'] -= int(quantity)
+    viewMenu[int(dish)-1]['stock'] -= int(quantity)
+    print(colored("Order has been placed!","green"))
 
-def update_snack():
-    item_id = int(input("Enter the ID of the snack to update: "))
-    for item in itemsList:
-        if item["id"] == item_id:
-            new_availability = input("Enter updated availability (Yes/No): ")
-            item["availability"] = new_availability.capitalize()
-            print("Snack availability updated successfully!\n")
-            return
-    print("Snack not found!\n")
+def updateStatus():
+    print("Enter the order ID to update status")
+    order = input(colored("Enter Order ID","blue"))
+    if (order not in orders) :
+        print(colored("Please enter valid ID!","red"))
+        return updateStatus()
+    print("1. Preparing")
+    print("2. Ready for pickup")
+    print("3. Delivered")
+    status = {
+        "1":"Preparing",
+        "2":"Ready for pickup",
+        "3":"Delivered"
+    }
+    userInput = input(colored("Choose an option: ","blue"))
+    orders[order]["status"] = status[userInput]
+    reviewOrders[int(order)-1]["status"] = status[userInput]
+    print(colored("Order Status has been updated!","green"))
 
-def sell_snack():
-    item_id = int(input("Enter the ID of the snack to remove: "))
-    for index, item in enumerate(itemsList):
-        if item["id"] == item_id:
-            if item["availability"] == "No":
-                print("This snack is not available and cannot be sold.\n")
-                return
-            soldSnacks.append(item)
-            del itemsList[index]
-            print("Snack Sold successfully!\n")
-            return
-    print("Snack not found!\n")
+def review() :
+    if len(reviewOrders) == 0 :
+        print(colored("No orders has been placed", "red"))
+        return operation()
+    headers = reviewOrders[0].keys()
+    table = tabulate([[row[col] for col in headers] for row in reviewOrders], headers, tablefmt="grid")
+    print(table)
 
-def view_sold_snacks():
-    print("***** Sold Snacks *****")
-    if len(soldSnacks) == 0:
-        print("No snacks have been sold yet.\n")
-    else:
-        for item in soldSnacks:
-            print(
-                f"ID: {item['id']}\tName: {item['name']}\tPrice: {item['price']}")
-        print()
+def menuu() :
+    if len(viewMenu) == 0 :
+        print(colored("No Dishes in menu!", "red"))
+        return operation()
+    headers = viewMenu[0].keys()
+    table = tabulate([[row[col] for col in headers] for row in viewMenu], headers, tablefmt="grid")
+    print(table)
 
-def inventory_management_app():
+def operation() :
     while True:
-        display_menu()
-        choice = input("Enter your choice (1-6): ")
-        print()
+        print("1. Add Dish")
+        print("2. Remove Dish")
+        print("3. Update Dish availability")
+        print("4. New Order")
+        print("5. Update Order Status")
+        print("6. Review Orders")
+        print("7. View Menu")
+        print(colored("0. Exit", "red"))
+        userInput = input(colored("Choose an option: ", "blue"))
 
-        if choice == "1":
-            view_inventory()
-        elif choice == "2":
-            add_snack()
-        elif choice == "3":
-            update_snack()
-        elif choice == "4":
-            sell_snack()
-        elif choice == "5":
-            view_sold_snacks()
-        elif choice == "6":
-            print("Exiting the application...")
+        if userInput == "1" :
+            add()
+        elif userInput == "2":
+            remove()
+        elif userInput == "3" :
+            update()
+        elif userInput == "4":
+            order()
+        elif userInput == "5":
+            updateStatus()
+        elif userInput == "6":
+            review()
+        elif userInput == "0":
+            print(colored("Thanks for visiting!","cyan"))
+            return
+        elif userInput == "7" :
+            menuu()
+        else :
+            print(colored("Please enter valid Input", "red"))
             break
-        else:
-            print("Invalid choice! Please try again.\n")
 
-inventory_management_app()
+operation()
